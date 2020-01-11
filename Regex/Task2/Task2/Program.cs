d9381cdcb9e7c120fcc4c9f824a1f55b8e7ba282
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-01-11 18:39:07 +0200

    Regex. Task 2 solved

--- Regex/Task2/Task2/Program.cs
+++ Regex/Task2/Task2/Program.cs
@@ -2,16 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Task2
 {
     class Program
     {
+        static void IPAddresses(string inputString)
+        {
+            Regex regex = new Regex(@"(^\d{2}|0x\w[a-f]).(1\d{2}|2[1-5][1-5]).(1\d{2}|2[1-5][1-5]).(\d{3}|0x\d{2})", RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(inputString);
+            if (matches.Count > 0)
+            {
+                Console.WriteLine("Found matches:");
+                foreach (Match match in matches)
+                    Console.WriteLine($"\t{match.Value}");
+            }
+            else
+            {
+                Console.WriteLine("Matches not found");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Please, enter IPv4 address in decimal or hex format");
 
-            Console.ReadKey();
+            while (true)
+            {
+                Console.Write("\nEnter IP address: ");
+                string date = Console.ReadLine();
+                IPAddresses(date);
+            }
         }
     }
 }
