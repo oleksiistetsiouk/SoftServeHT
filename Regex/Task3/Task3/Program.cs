7fee8cf9cf66bdef9e2d369ef6daed0af180cb8b
Oleksii Stetsiouk <oleksii.stetsiouk@gmail.com>
2020-01-15 15:14:49 +0200

    Task 3 solved

---
+++ Regex/Task3/Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string @string = null;
+
+            using (StreamReader sr = new StreamReader(@"text.txt"))
+            {
+                @string = sr.ReadToEnd();
+            }
+
+            Console.WriteLine(@string);
+
+            string pattern = @"(.*)(\r?\n\1)";
+            string target = "$1\n";
+            Regex regex = new Regex(pattern);
+            string result = regex.Replace(@string, target);
+
+            Console.WriteLine($"\n\nResult:\n{result}");
+
+            Console.ReadKey();
+        }
+    }
+}
